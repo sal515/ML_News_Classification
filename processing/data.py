a4c98abb0b5291e34de27e4322602c0b608ca363
sal515 <salman.rahman515@gmail.com>
2020-06-05 14:51:11 -0400

    Completed pre-processing and freq distribution

--- processing/data.py
+++ processing/data.py
@@ -1,77 +1,99 @@
-import re
-import time
+# import re
+# import time
 import nltk
-import string
+import numpy as np
 import pandas as pd
 
 
-def preprocess_regex(sentences):
-    clean_sentence = []
-    # for sentence in (sentences[:][0]):  # this is Df_pd for Df_np (text[:])
-    for sentence in sentences:  # this is Df_pd for Df_np (text[:])
-        new_sentence = re.sub("<.*?>", "", sentence)  # remove HTML tags
-        new_sentence = re.sub("[^\w\s]", "", new_sentence)  # remove punc.
-        new_sentence = re.sub("\d+", "", new_sentence)  # remove numbers
-        # new_sentence = new_sentence.lower()  # lower case, .upper() for upper
-        if new_sentence != "":
-            clean_sentence.append(new_sentence)
-    return clean_sentence
+# import string
+
+
+# def preprocess_regex(sentences):
+#     clean_sentence = []
+#     # for sentence in (sentences[:][0]):  # this is Df_pd for Df_np (text[:])
+#     for sentence in sentences:  # this is Df_pd for Df_np (text[:])
+#         new_sentence = re.sub("<.*?>", "", sentence)  # remove HTML tags
+#         new_sentence = re.sub("[^\w\s]", "", new_sentence)  # remove punc.
+#         new_sentence = re.sub("\d+", "", new_sentence)  # remove numbers
+#         # new_sentence = new_sentence.lower()  # lower case, .upper() for upper
+#         if new_sentence != "":
+#             clean_sentence.append(new_sentence)
+#     return clean_sentence
 
 
 def preprocess_translate(sentences):
     clean_sentence = []
-    excluded_punc = string.punctuation
+    # excluded_punc = string.punctuation
     # for sentence in (sentences[:][0]):  # this is Df_pd for Df_np (text[:])
     for sentence in sentences:  # this is Df_pd for Df_np (text[:])
-        new_sentence = sentence.translate(str.maketrans('', '', string.punctuation))
-        new_sentence = new_sentence.translate(str.maketrans('', '', string.digits))
+        new_sentence = sentence.translate(str.maketrans('', '', '!"#$%&\'()*+,./:;<=>?@[\\]^_`{|}~'))
+        new_sentence = new_sentence.translate(str.maketrans('', '', '0123456789'))
+        # new_sentence = sentence.translate(str.maketrans('', '', string.punctuation))
+        # new_sentence = new_sentence.translate(str.maketrans('', '', string.digits))
         # new_sentence = re.sub(" +", " ", new_sentence)
-        new_sentence = re.sub("[^\w\s]", "", new_sentence)  # remove punc.
-        # new_sentence = re.sub("[^\s]", "", new_sentence)  # remove punc.
+        # new_sentence = re.sub("[^\w\s]", "", new_sentence)  # remove punc.
 
         if new_sentence != "":
             clean_sentence.append(new_sentence)
     return clean_sentence
 
 
-def clean_tokenize_freq(strings_list):
+def clean_tokenize(strings_list, combine):
     tokenized = []
-    freq_dist = []
-
     cleaned_string = preprocess_translate(strings_list)
     for s in cleaned_string:
-        t = nltk.word_tokenize(s)
-        tokenized.append(t)
-        freq_dist.append(nltk.FreqDist(t))
-    return tokenized, freq_dist
+        tokenized.append(nltk.word_tokenize(s))
+    if combine:
+        tokenized = np.concatenate(tokenized)
+
+    # print(tokenized, tokenized.__len__())
+    return tokenized
+
+
+def frequency_distribution(tokenized_arr):
+    if isinstance(tokenized_arr, list) and isinstance(tokenized_arr[0], list):
+        freq = [dict(nltk.FreqDist(t)) for t in tokenized_arr]
+        for e in freq:
+            if "-" in e:
+                del e["-"]
+        return freq
+
+    freq = dict(nltk.FreqDist(tokenized_arr))
+    del freq["-"]
+    return freq
 
 
 # TEST CODE
 if __name__ == "__main__":
     import pandas as pd
 
-    data_set = pd.read_csv("../data/hns_2018_2019.csv", encoding="utf-8")
-    stop_words = pd.read_csv("../data/stopwords.txt")
+    training_data = pd.read_csv("../data/hns_2018_2019.csv", encoding="utf-8")
+    stop_words = pd.read_csv("../data/stopwords.txt", encoding="utf-8")
     # print(data_set)
     # print(stop_words)
     # data_set.info()
 
-    data_set["Title"] = list(data_set["Title"].str.lower())
-    data_set["Post Type"] = list(data_set["Post Type"].str.lower())
+    td_bckup = training_data
+
+    training_data["Title"] = training_data["Title"].str.lower()
+    training_data["Post Type"] = training_data["Post Type"].str.lower()
 
     # [print(i) for i in data_set["Title"]]
     # [print(i) for i in data_set["Post Type"]]
     # quit(-1)
 
-    sentences_list = list(data_set["Title"])
+    sentences_list = list(training_data["Title"])
 
     # timer_start = time.perf_counter()
-    words_tokenized, words_freq = clean_tokenize_freq(sentences_list)
+    vocabulary = clean_tokenize(sentences_list, combine=True)
+    vocabulary_freq = frequency_distribution(vocabulary)
     # time_taken = time.perf_counter() - timer_start
     # print("translating t1 : ", time_taken)
 
-    post_types_list = list(data_set["Post Type"])
-    post_freq = dict(nltk.FreqDist(post_types_list))
+    # quit(-1)
+
+    post_types_list = list(training_data["Post Type"])
+    post_freq = nltk.FreqDist(post_types_list)
 
     # clean_tokenize_freq(sentences)
     #
